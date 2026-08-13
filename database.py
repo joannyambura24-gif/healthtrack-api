c9ebfbeb4203a803bcf6d2911bcdb0439c7aec86
joannyambura24-gif <joan.nyambura24@students.dkut.ac.ke>
2026-08-13 16:33:34 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Session, create_engine
+
+DATABASE_URL = "sqlite:///techvault.db"
+
+engine = create_engine(DATABASE_URL)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
